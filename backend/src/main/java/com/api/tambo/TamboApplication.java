2604b7f0b0fbab02c2d51e724114de25c5e0bf7e
Thalis Antunes <thalisantunes@hotmail.com>
2020-09-27 19:28:36 -0300

    Upload file method

--- backend/src/main/java/com/api/tambo/TamboApplication.java
+++ backend/src/main/java/com/api/tambo/TamboApplication.java
@@ -1,13 +1,38 @@
 package com.api.tambo;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootApplication
+@RestController
 public class TamboApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(TamboApplication.class, args);
+	}
+
+
+
+	@RequestMapping(value="/upload", method=RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+		File convertFile = new File("C:\\Users\\thali\\Desktop\\Tambo\\backend\\src\\main\\java\\com\\api\\tambo"+file.getOriginalFilename());
+		convertFile.createNewFile();
+		FileOutputStream fout = new FileOutputStream(convertFile);
+		fout.write(file.getBytes());
+		fout.close();
+		return new ResponseEntity<>("File is uploaded successfully", HttpStatus.OK);
 	}
 
 }
